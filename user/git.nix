97b3afa647de9c5b6ba63bfa0e8cff5c7d5e1975
Ollie Relph <ollie@relph.me>
2023-01-26 15:16:32 +0000

    nix-shaped git

--- user/git.nix
+++ user/git.nix
@@ -2,39 +2,35 @@
 {
   enable = true;
 
-  userName = "Ollie Relph";
-  userEmail = "ollie@relph.me";
+  includes = [
+    {
+      condition = "gitdir:~/dev/olx/";
+      path = "~/dev/olx/.gitconfig";
+    }
+  ];
 
-  extraConfig = ''
-    [core]
-      excludesfile = ~/.gitignore
-      pager = diff-so-fancy | less --tabs=4 -RFX
-      autocrlf = input
-    [includeIf "gitdir:~/dev/olx/"]
-      path = ~/dev/olx/.gitconfig
-    [includeIf "gitdir:~/dev/hsbc/"]
-      path = ~/dev/hsbc/.gitconfig
-    [push]
-      default = simple
-    [filter "lfs"]
-      clean = git-lfs clean %f
-      smudge = git-lfs smudge %f
-      required = true
-    [color]
-      ui = true
-    [color "diff-highlight"]
-      oldNormal = red bold
-      oldHighlight = red bold 52
-      newNormal = green bold
-      newHighlight = green bold 22
-    [color "diff"]
-      meta = yellow
-      frag = magenta bold
-      commit = yellow bold
-      old = red bold
-      new = green bold
-      whitespace = red reverse
-    [pull]
-      rebase = true
-  '';
+  diff-so-fancy = {
+    enable = true;
+  };
+
+  extraConfig = {
+    user = {
+      email = "ollie@relph.me";
+      name = "Ollie Relph";
+    };
+
+    core = {
+      excludesfile = "~/.gitignore";
+      autocrlf = "input";
+    };
+
+    push = {
+      default = "simple";
+    };
+
+    pull = {
+      rebase = true;
+    };
+
+  };
 }
