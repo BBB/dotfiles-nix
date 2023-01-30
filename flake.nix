fee6174f5aea897fc77f32f7234f00f9430b0f9f
Ollie Relph <ollie@relph.me>
2023-01-30 09:01:58 +0000

    Add formatter

--- flake.nix
+++ flake.nix
@@ -1,24 +1,24 @@
 {
-    description = "My Home Manager Flake";
+  description = "My Home Manager Flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = {nixpkgs, home-manager, ...}: {
-        defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+  outputs = { nixpkgs, home-manager, ... }: {
+    defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
+    homeConfigurations = {
+      "ollie" = home-manager.lib.homeManagerConfiguration {
+        # Note: I am sure this could be done better with flake-utils or something
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
 
-        homeConfigurations = {
-            "ollie" = home-manager.lib.homeManagerConfiguration {
-                # Note: I am sure this could be done better with flake-utils or something
-                pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-
-                modules = [ ./user/home.nix ];
-            };
-        };
+        modules = [ ./user/home.nix ];
+      };
     };
+  };
 }
