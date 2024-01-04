72073d5485ca3c125603dfd8a4390991f03eb298
Ollie Relph <ollie@relph.me>
2024-01-04 11:49:12 +0000

    protect on update

--- user/zsh/init.sh
+++ user/zsh/init.sh
@@ -1,3 +1,11 @@
 
+# Nix
+# OSX often removes this from /etc/zshrc on a software update, so
+# putting it here ensures it's persisted
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+# End Nix
+
 # zsh-autosuggestions colours
 if [ ! "$TMUX" = "" ]; then export TERM=xterm-256color; fi
